2616a7b4fd1f6cdf06f72a5007bf75bd6d54e7d2
JuanArevalo7 <jnarevalo74@gmail.com>
2025-05-07 15:03:44 -0500

    mongo usando javascript

---
+++ ing datos/proyectoExpress/routes/productoRoutes.js
@@ -0,0 +1,14 @@
+const express=requiere('express');
+const router=express.router();
+const item=requiere("../models/producto")
+//registrar un producto
+
+//consultar todos los productos
+
+//consultar por ID
+
+//modificar datos del producto
+
+//eliminar un productoñ
+
+module.exports=router;
